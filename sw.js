a93c8cbf295aa1c6bc7108dfde58290a97381fe6
Scott Westover <scottwestover2006@gmail.com>
2018-02-11 00:08:50 -0500

    Site updated: 2018-02-11 00:08:50

--- sw.js
+++ sw.js
@@ -32,9 +32,5 @@ toolbox.precache(precacheUrls);
 toolbox.options = {"networkTimeoutSeconds":5};
 
 
-toolbox.router.any(/hm.baidu.com/, toolbox.networkOnly);
-
 toolbox.router.any(/.*apollo\.(js|css|jpg|jpeg|png|gif)$/, toolbox.cacheFirst);
-
-toolbox.router.any(/\//, toolbox.networkFirst);
 
